86a0ac28e0e33ec196838de9f5d6475feb7e8d3c
Mary Griffus <mary.e.shultz@gmail.com>
2016-07-05 13:20:48 -0400

    adding authentication controls

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+
+  before_filer :authenticate_user!, except [:show] 
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+def post_params
+  params.require(:post).permit(:title, :body, :user)
+end
+
 end
